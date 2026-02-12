f23054d95ef45059630009a2523746036d6b6381
ChrolloKyber <rishay.raghav00@gmail.com>
2026-02-12 22:27:23 +0530

    add niri conf

--- config/hypr/scripts/saveScreenshot.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-location=~/Pictures/Screenshots/Screenshot_$(date +%Y%m%d-%H%M%S).png
-grimblast save area "${location}" && swappy -f "${location}" && notify-send -a "Grimblast" "Screenshot saved" -t 2000 -u low -r 85
--- config/waybar/scripts/Apps.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env zsh
-rofi -show drun -show-icons
